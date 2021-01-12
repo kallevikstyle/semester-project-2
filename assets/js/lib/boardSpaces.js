d69c2077e137370e8d015012b37fb6d070a517cf
Asbjørn Kallevik <44470241+kallevikstyle@users.noreply.github.com>
2021-01-13 00:43:50 +0100

    Working with boardSpaces.js

--- assets/js/lib/boardSpaces.js
+++ assets/js/lib/boardSpaces.js
@@ -1,17 +1,23 @@
 // Coordinates and dimensions for board spaces
-const spaces = {
+const spaceDetails = {
     xAxisLeft: 329.8876,
     xAxisRight: 929.3828,
     yAxisTop: 232.841,
     yAxisBottom: 454.7887,
-    standardWidth: 32.7542,
-    specialWidth: 43.8783
-};
+    largeWidth: 32.7542,
+    standardWidth: 43.8783
+    },
+    spaces = [
+        {id: 0, large: false, x: spaceDetails.xAxisLeft, y: spaceDetails.yAxisBottom}
+    ];
     
     
 export const createBoardSpaces = {
     displaySpaces: function(guideline) {
-        
+        // Dilemma:
+        // Bruke absolutte koordinater på gameboard
+        // eller
+        // Bruke relative koordinater fra guideline div
     },
     start: function() {
         // Create guideline for spaces
@@ -23,10 +29,10 @@ export const createBoardSpaces = {
         boardSpaces.addClass('gameboard__spaces');
         // Give guideline dimentions and positioning
         boardSpaceContainer.css({
-            "width": `${spaces.xAxisRight - spaces.xAxisLeft}px`,
-            "height": `${spaces.yAxisBottom - spaces.yAxisTop}px`,
-            "top": `${spaces.yAxisTop}px`,
-            "left": `${spaces.xAxisLeft}px`
+            "width": `${spaceDetails.xAxisRight - spaceDetails.xAxisLeft}px`,
+            "height": `${spaceDetails.yAxisBottom - spaceDetails.yAxisTop}px`,
+            "top": `${spaceDetails.yAxisTop}px`,
+            "left": `${spaceDetails.xAxisLeft}px`
         });
        
         boardSpaceContainer.append(boardSpaces);

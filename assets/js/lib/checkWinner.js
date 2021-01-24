b5a9a20f537f5c71bdd7ccf5a7a7cf7a0af49126
Asbjørn Kallevik <44470241+kallevikstyle@users.noreply.github.com>
2021-01-24 02:21:51 +0100

    Storing game winner in localStorage

--- assets/js/lib/checkWinner.js
+++ assets/js/lib/checkWinner.js
@@ -4,12 +4,17 @@ export function checkWinner(game)  {
         && game.player1.army >= game.armyToWin 
         && game.spaces[game.player1.space].zone === "kingslanding") {
             // Player 1 is the winner
+            localStorage.clear();
+            localStorage.setItem('winner', game.player1.name);
+
             console.log("Player1 is da winner!!");
             game.turn.updateNarrative(game.player1, game.spaces[game.player1.space].zoneText, null);
         } else if (!game.turn.player1 
             && game.player2.army >= game.armyToWin 
             && game.spaces[game.player2.space].zone === "kingslanding") {
                 // Player 2 is the winner
+                localStorage.clear();
+                localStorage.setItem('winner', game.player1.name);
                 console.log("Player2 is da winner!!");
                 game.turn.updateNarrative(game.player2, game.spaces[game.player1.space].zoneText, null);
             }
